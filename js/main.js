8d38f84a70079e01fd5f57003cbd03b61a371f8e
vtnboolean <vnthuanx@gmail.com>
2022-06-22 16:49:56 +0200

    second control added

--- js/main.js
+++ js/main.js
@@ -25,52 +25,58 @@ function turnString(word){
 
 const userChoice=prompt("Scegli pari o dispari");
 
-let pariIndex = userChoice.indexOf('pari');
-let dispariIndex = userChoice.indexOf('dispari');
 
-if (pariIndex != -1 || dispariIndex != -1) {  
+if (userChoice=="pari"  || userChoice == "dispari") {  
     console.log(`Hai scelto ${userChoice}`);
-    const userNumber=parseInt(prompt("Scegli un numero da 1 a 5"));
+    
 
-    console.log (`Numero scelto: ${userNumber}`);
-    const aiNumber = random();
+    for (let i=0; i <1; i++){
+        const userNumber=parseInt(prompt("Scegli un numero da 1 a 5"));
+        if ((userNumber > 0) && (userNumber<6)){
 
-    function random(){      
-        let number=Math.floor(Math.random() * 5) + 1;
-        console.log(`Numero random= ${number}`);
-        return number;
-    }    
-
-    let sum = aiNumber + userNumber;
-    console.log(`la Somma = ${sum}`);
-
-
-    let evenOrOdd = whatSum(sum);
-
-    function whatSum(sum){
-        let result;
-        if (sum % 2 ==0){
-            result = "pari";
-            console.log(`la somma è ${result}`);        
+        console.log (`Numero scelto: ${userNumber}`);
+        
+        const aiNumber = random();
+    
+        
+        let sum = aiNumber + userNumber;
+        console.log(`la Somma = ${sum}`);
+    
+    
+        let evenOrOdd = whatSum(sum);
+    
+        if (evenOrOdd==userChoice){
+            console.log("Hai vinto!!!")
         } else {
-            result = "dispari";
-            console.log(`la somma è ${result}`);
+            console.log("Hai perso!!!")
         }
-        return result;
-    }
+            
+        } else {
+            console.log('input non valido');
+        }
 
-    if (evenOrOdd==userChoice){
-        console.log("Hai vinto!!!")
-    } else {
-        console.log("Hai perso!!!")
-    }
+    } 
 
-
-   } else {
+}  else {
     console.log('input non valido');
-   }
+}
 
 
+function random(){      
+    let number=Math.floor(Math.random() * 5) + 1;
+    console.log(`Numero random= ${number}`);
+    return number;
+}    
 
 
-
+function whatSum(sum){
+    let result;
+    if (sum % 2 ==0){
+        result = "pari";
+        console.log(`la somma è ${result}`);        
+    } else {
+        result = "dispari";
+        console.log(`la somma è ${result}`);
+    }
+    return result;
+}
